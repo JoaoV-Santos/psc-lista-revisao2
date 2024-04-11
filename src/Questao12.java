4420161df050dff14d67b1378bc05f117776df86
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-11 15:49:01 -0300

    questao 12 resolvida

--- src/Questao12.java
+++ src/Questao12.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Questao12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Digite uma palavra: ");
+        String palavra = in.nextLine();
+        String palindromo = new StringBuilder(palavra).reverse().toString();
+        if (palindromo.equals(palavra))
+            System.out.println("É um palíndromo!");
+        else
+            System.out.println("Não é um palíndromo!");
+
+        in.close();
+    }
 }
