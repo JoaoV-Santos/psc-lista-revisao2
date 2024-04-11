8d541956c819fd43a2d72e323f4df00d26069fb8
João Victor <joaovictorsantos10@hotmail.com.br>
2024-04-11 15:01:45 -0300

    questao 08 resolvida

---
+++ src/Questao08.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Questao08 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Informe um número:");
+        int n = in.nextInt();
+        System.out.println((n % 2 == 0) ? "Par" : "Ímpar");
+    }
+}
